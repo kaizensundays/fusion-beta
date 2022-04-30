55d8b508048ed1a80b5cc5703a87f6639bd79a37
kaizensundays <15171090+kaizensundays@users.noreply.github.com>
2022-04-30 15:21:18 -0400

    [ fusion-lib ] (#2)

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,10 @@ subprojects {
     }
 
     val log4jVersion = "2.17.1"
+    val jacksonVersion = "2.12.6"
+    val ktorVersion = "1.6.7"
+    val igniteVersion = "2.11.0"
+    val postgreSqlVersion = "42.3.4"
 
     dependencyManagement {
         dependencies {
@@ -26,6 +30,21 @@ subprojects {
             dependency("org.apache.logging.log4j:log4j-core:$log4jVersion")
             dependency("org.apache.logging.log4j:log4j-jcl:$log4jVersion")
             dependency("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
+
+            dependency("com.fasterxml.jackson.core:jackson-core:$jacksonVersion")
+            dependency("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
+            dependency("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+
+            dependency("io.ktor:ktor-client-cio:$ktorVersion")
+            dependency("io.ktor:ktor-client-websockets:$ktorVersion")
+            dependency("io.ktor:ktor-client-auth-basic-jvm:$ktorVersion")
+
+            dependency("org.postgresql:postgresql:$postgreSqlVersion")
+
+            dependency("org.apache.ignite:ignite-core:$igniteVersion")
+            dependency("org.apache.ignite:ignite-slf4j:$igniteVersion")
+            dependency("org.apache.ignite:ignite-spring:$igniteVersion")
+            dependency("com.h2database:h2:1.4.197")
         }
     }
 }
---
+++ fusion/fusion-lib/src/test/java/com/kaizensundays/fusion/ktor/SomeTest.kt
@@ -0,0 +1,17 @@
+package com.kaizensundays.fusion.ktor
+
+import org.junit.Test
+
+/**
+ * Created: Sunday 4/17/2022, 5:18 PM Eastern Time
+ *
+ * @author Sergey Chuykov
+ */
+class SomeTest {
+
+    @Test
+    fun test() {
+
+    }
+
+}
