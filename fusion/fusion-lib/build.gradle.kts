be58e42810e3398498faafd623a80194376bd0a9
KaizenSundays <kaizensundays@localhost>
2022-04-30 15:20:47 -0400

    [ fusion-lib ]

--- fusion/fusion-lib/build.gradle.kts
+++ fusion/fusion-lib/build.gradle.kts
@@ -10,12 +10,28 @@ java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-common")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     implementation("org.apache.logging.log4j:log4j-api")
     implementation("org.apache.logging.log4j:log4j-core")
     implementation("org.apache.logging.log4j:log4j-jcl")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl")
+
+    implementation("com.fasterxml.jackson.core:jackson-core")
+    implementation("com.fasterxml.jackson.core:jackson-databind")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    implementation("io.ktor:ktor-client-cio")
+    implementation("io.ktor:ktor-client-websockets")
+    implementation("io.ktor:ktor-client-auth-basic-jvm")
+
+    implementation("org.apache.ignite:ignite-core")
+    implementation("org.apache.ignite:ignite-slf4j")
+    implementation("org.apache.ignite:ignite-spring")
+    implementation("com.h2database:h2")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
     testImplementation("org.mockito.kotlin:mockito-kotlin:3.2.0")
